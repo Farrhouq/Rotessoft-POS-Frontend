a96e85a53cbb8ebf18bd3f59bd33535ece875fae
Farouq <farouqimoro@gmail.com>
2024-10-03 14:29:39 +0000

    clean up side bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import React, { useEffect } from 'react';
-import {
-  Routes,
-  Route,
-  useLocation
-} from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 
-import './css/style.css';
+import "./css/main.css";
 
-import './charts/ChartjsConfig';
+import "./charts/ChartjsConfig";
 
 // Import pages
-import Dashboard from './pages/Dashboard';
+import Dashboard from "./pages/Dashboard";
+import SlideBar from "./partials/Sidebar";
 
 function App() {
-
   const location = useLocation();
 
   useEffect(() => {
-    document.querySelector('html').style.scrollBehavior = 'auto'
-    window.scroll({ top: 0 })
-    document.querySelector('html').style.scrollBehavior = ''
+    document.querySelector("html").style.scrollBehavior = "auto";
+    window.scroll({ top: 0 });
+    document.querySelector("html").style.scrollBehavior = "";
   }, [location.pathname]); // triggered on route change
 
   return (
-    <>
+    <div className="flex h-screen overflow-hidden">
+      <SlideBar />
       <Routes>
         <Route exact path="/" element={<Dashboard />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
