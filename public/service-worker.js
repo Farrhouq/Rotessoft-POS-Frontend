34be7f23de47b5d4a4426c5e9d325e8cbae65ae6
Farouq <farouqimoro@gmail.com>
2024-10-28 22:15:52 +0000

    service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,14 +4,39 @@ self.addEventListener("install", (event) => {
       return cache.addAll([
         "/",
         "/index.html",
-        "/main.css", // Add paths to your CSS files
-        // "/js/app.js", // Add paths to your JS files
+        // "/main.css", // Add paths to your CSS files
+        "/app.js", // Add paths to your JS files
         // "/icons/icon-192x192.png", // Add paths to your icons
         // "/icons/icon-512x512.png",
         "/rotessoft-icon.png",
       ]);
     }),
   );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.url.includes("/assets/")) {
+    // Handle requests to the assets folder
+    event.respondWith(
+      caches.open("v1").then((cache) => {
+        return cache.match(event.request).then((response) => {
+          return (
+            response ||
+            fetch(event.request).then((fetchResponse) => {
+              cache.put(event.request, fetchResponse.clone());
+              return fetchResponse;
+            })
+          );
+        });
+      }),
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      }),
+    );
+  }
 });
 
 self.addEventListener("fetch", (event) => {
