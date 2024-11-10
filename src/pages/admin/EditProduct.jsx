694fae5e37bcac430db035c781a4f2afda095445
Farouq <farouqimoro@gmail.com>
2024-11-10 21:57:07 +0000

    edit product stock

--- src/pages/admin/EditProduct.jsx
+++ src/pages/admin/EditProduct.jsx
@@ -223,7 +223,6 @@ const EditProductForm = () => {
               value={stock}
               onChange={(e) => setStock(e.target.value)}
               className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-purple-500 focus:border-purple-500 sm:text-sm dark:bg-gray-700 dark:border-gray-600 dark:text-white"
-              readOnly
             />
           </div>
         </div>
