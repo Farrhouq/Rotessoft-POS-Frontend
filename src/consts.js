8553d7b1c8987aaf08be638f792dbc0fc766e934
Farrhouq <farouqimoro@gmail.com>
2025-06-22 22:30:06 +0000

    update backend url

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const BACKEND_URL =
   import.meta.env.VITE_BACKEND_ENV === "local"
     ? "http://127.0.0.1:8000"
-    : "https://farrhouqbnb.tech";
+    : "https://rotessoft-pos-backend.vercel.app";
 export const FRONTEND_URL = "http://localhost:3000";
